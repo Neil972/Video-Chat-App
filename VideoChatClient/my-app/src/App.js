64fb0129cffe7bcdfabb78183f3a2a2c21cf61b2
Indranil Ghosh <indranilghosh896@gmail.com>
2023-08-02 16:38:58 +0530

    Update App.js

--- VideoChatClient/my-app/src/App.js
+++ VideoChatClient/my-app/src/App.js
@@ -4,7 +4,7 @@ import React from 'react'
 import {io} from 'socket.io-client'
 import Peer from 'simple-peer'
 
-let socket=io('http://localhost:5000')
+let socket=io('https://video-w1np.onrender.com')
 
 class App extends React.Component{
  constructor(props){
